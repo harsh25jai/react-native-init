c7f505f9d75e6b2dc70e1ace11b22397df6e4ea2
Harsh Jaiswal <harshjaiswal@MacBook-Pro-19.local>
2024-11-08 13:23:32 +0530

    feat: Added eslintric support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,37 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // this is for sorting WITHIN an import
+    'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: true }],
+    // this is for sorting imports
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
